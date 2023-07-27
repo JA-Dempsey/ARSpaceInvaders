afb1049cd7bacda738ff6694401a05ad91fd3d6e
Chris Nguyen <cnguyen0320@gmail.com>
2023-07-27 07:01:54 -0700

    move health check after collision

--- Assets/Scripts/HealthDestructable.cs
+++ Assets/Scripts/HealthDestructable.cs
@@ -12,16 +12,7 @@ public class HealthDestructable : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // if health falls below 0, destroy the object
-        if(health <= 0.0){
-
-            // play explosion animation
-            if(explosion != null){
-                GameObject destruction_animation = Instantiate(explosion, this.gameObject.transform.position, Quaternion.identity);
-                destruction_animation.transform.localScale *= explosion_scale; 
-            }
-            Destroy(this.gameObject);
-        }
+        
     }
 
     private void OnCollisionEnter(Collision other) {
@@ -30,6 +21,18 @@ public class HealthDestructable : MonoBehaviour
         if(other.gameObject.tag == "Projectile"){
             // TODO deduct projectile strength from object
             health -= 1.0f;
+
+
+            // if health falls below 0, destroy the object
+            if(health <= 0.0){
+
+                // play explosion animation
+                if(explosion != null){
+                    GameObject destruction_animation = Instantiate(explosion, this.gameObject.transform.position, Quaternion.identity);
+                    destruction_animation.transform.localScale *= explosion_scale; 
+                }
+                Destroy(this.gameObject);
+            }
         }
     }
 
