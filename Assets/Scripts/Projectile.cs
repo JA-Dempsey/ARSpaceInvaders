4386d2e0b8bf3b7c86540cff046898d7fc5c4a20
Chris Nguyen <cnguyen0320@gmail.com>
2023-07-30 21:53:46 -0700

    move player under camera to allow for tracking with camera

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEditor;
-using UnityEditor.XR.Management;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
